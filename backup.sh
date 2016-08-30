7ab5179fa62d94f06ec9ae544d1e1795b530f0fa
ram <rogue07@users.noreply.github.com>
2016-08-29 22:17:03 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,7 +11,7 @@
 
 VER="v2.01"
 LOG="/tmp/backup.log"
-RAM="3523969626"
+ME="1234567890"
 HOSTNAME=$(hostname)
 TIMESTAMP=$(date +%Y%m%d_%H%M)
 
@@ -45,21 +45,21 @@ date | tee -a $LOG
 if [[ "$HOSTNAME" == "pbody" ]]; then
 	echo
 	echo "This is pbody, scp'ing the backup" | tee -a $LOG
-	scp -v ~/*.tar shimmernshine@192.168.10.201:/run/media/shimmernshine/Tarantula/backups/ | tee -a $LOG
+	scp -v ~/*.tar box1@192.168.10.2:/run/media/box1/backups/ | tee -a $LOG
 		if [ "$?" = "0" ]; then
 			echo
 			echo -e "Scp of file successful" | tee -a $LOG
 		else
 			echo
 			echo -e "Scp failure, it's broke." | tee -a $LOG
-curl http://textbelt.com/text -d "number=$CELL" -d "message=$HOSTNAME scp failed. Backup failed." |tee -a $LOG
+curl http://textbelt.com/text -d "number=$ME" -d "message=$HOSTNAME scp failed. Backup failed." |tee -a $LOG
 			exit 1
 		fi
 else
 	echo
-	echo "This is shimmer, copying to external." | tee -a $LOG
-	mv -v ~/*.tar /run/media/shimmernshine/Tarantula/backups/ | tee -a $LOG
-	find /run/media/shimmernshine/Tarantula/backups/*backup*.tar -mtime +14 -exec rm {} \; | tee -a $LOG
+	echo "This is box1, copying to external." | tee -a $LOG
+	mv -v ~/*.tar /run/media/box1/backups/ | tee -a $LOG
+	find /run/media/box1/backups/*backup*.tar -mtime +14 -exec rm {} \; | tee -a $LOG
 fi
 
 ### Remove backup files that were created ###
@@ -71,4 +71,4 @@ rm -rfv ~/*backup*.tar | tee -a $LOG
 ### All is well, send a verification text. ###
 echo
 echo "Everything went well, sending text." | tee -a $LOG
-curl http://textbelt.com/text -d "number=$RAM" -d "message=$HOSTNAME backup is complete. $SIZE MB" | tee -a $LOG
+curl http://textbelt.com/text -d "number=$ME" -d "message=$HOSTNAME backup is complete. $SIZE MB" | tee -a $LOG
